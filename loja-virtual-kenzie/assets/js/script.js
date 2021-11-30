724c9c08c91efbe32c91a6b8c8b08f21c21351ed
Hudson Carolino <hudson@kenzie.com.br>
2021-11-30 11:07:37 -0300

    Demo de revisao (Dom+Eventos+Bubling+Capturing)

---
+++ loja-virtual-kenzie/assets/js/script.js
@@ -0,0 +1,80 @@
+const vitrineCarrinho = document.querySelector(".carrinho ul")
+
+//const cardProduto  = document.querySelectorAll(".vitrineProdutos__vitrine li button")
+
+// for(let i = 0; i<cardProduto.length; i++){
+    
+//     cardProduto[i].addEventListener("click", adicionarProdutoCarrinho)
+
+// }
+
+// cardProduto.forEach(function(buttonComprar){
+//     buttonComprar.addEventListener("click", adicionarProdutoCarrinho)
+// })
+
+
+//CONCEITO DE CAPTURING 
+//cardProduto.forEach((buttonComprar)=> buttonComprar.addEventListener("click", adicionarProdutoCarrinho))
+
+
+const vitrineProdutos  = document.querySelector(".vitrineProdutos__vitrine")
+vitrineProdutos.addEventListener("click", interceptandoEvento)
+
+function interceptandoEvento(evt){
+    
+    //evt.target//CAPTURING
+    //evt.currentTarget//Bubling
+    const buttonComprar = evt.target 
+    if(buttonComprar.tagName === "BUTTON"){
+
+       const cadProduto = buttonComprar.closest("li")
+        addicionarProdutoCarrinho(cadProduto,vitrineCarrinho)
+
+    }
+    
+}
+function addicionarProdutoCarrinho(produto,carrinho){
+    carrinho.appendChild(produto)
+}
+
+const div1 = document.getElementById("div1")
+div1.addEventListener("click", function(){
+    setTimeout(() => {
+        div1.style.background = "blue"
+    }, 1200);   
+})
+
+const div2 = document.getElementById("div2")
+div2.addEventListener("click", function(){
+    setTimeout(() => {
+        div2.style.background = "blue"
+    }, 1000);
+})
+
+const div3 = document.getElementById("div3")
+div3.addEventListener("click", function(evt){
+    
+    setTimeout(() => {
+        div3.style.background = "blue"
+    }, 900);
+
+    
+})
+
+const div4 = document.getElementById("div4")
+div4.addEventListener("click", function(evt){
+    setTimeout(() => {
+        div4.style.background = "blue"
+    }, 800);
+    evt.stopPropagation()
+})
+
+const div5 = document.getElementById("div5")
+div5.addEventListener("click", function(){
+    setTimeout(() => {
+        div5.style.background = "blue"  
+    }, 700);
+   
+})
+
+
