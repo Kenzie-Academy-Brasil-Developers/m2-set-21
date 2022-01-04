138c43c2301536ef938f7af19eea06a6fea63c23
Hudson Carolino <hudson@kenzie.com.br>
2022-01-04 13:59:38 -0300

    tecnicas de teste + tdd

---
+++ TDD/index.js
@@ -0,0 +1,58 @@
+import {Funcionario} from './src/models/Funcionario.js'
+import {Instrutor} from './src/models/Instrutor.js'
+import {Instituicao} from './src/models/Instituicao.js'
+
+
+    // const funcionarioHudson = new Funcionario(
+    //     "Hudson", 
+    //     "Carolino",
+    //     "hudson@gmail.com",
+    //     "108.266.049-32",
+    //     "31/10/1996",
+    // )
+
+    //INSTRUTOR
+    // const novoInstrutorHudson = new Instrutor(
+    //     "Hudson", 
+    //     "Carolino",
+    //     "hudson@gmail.com",
+    //     "108.266.049-32",
+    //     "31/10/1996",
+    //     "m2"
+    // )
+
+    // const novoInstrutorIsa = new Instrutor(
+    //     "Isa", 
+    //     "Bertine",
+    //     "isa@gmail.com",
+    //     "108.266.049-32",
+    //     "31/10/1996",
+    //     "m1"
+    // )
+    
+  
+    // //ESCOLA
+    // const kenzie  = new Instituicao("Kenzie",
+    //     [
+    //         {nome: "m1"},
+    //         {nome: "m2"},
+    //         {nome: "m3"},
+    //     ], 
+    //     ["Front-end", "CSS Avançado"] 
+    // )
+
+
+    // kenzie.addNovoInstrutor(novoInstrutorHudson)
+    // kenzie.addNovoInstrutor(novoInstrutorIsa)
+    // console.log(kenzie)
+  
+
+
+
+
+
+
+
+
+
+
