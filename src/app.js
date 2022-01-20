9d8180275359752a02a04eac47830e4040c19eef
Pedro Costa <phmc99@outlook.com>
2022-01-20 16:03:00 -0300

    ajuste

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = require("./routes/movies");
+const cors = require("cors")
 
 const app = express();
+app.use(cors());
 app.use(express.json())
 app.use("/movies", router)
 
--- src/server.js
+++ src/server.js
@@ -2,4 +2,4 @@ const app = require("./app");
 
 const port = 3000
 
-app.listen(port, () => console.log(`App running on http://localhost:${port}`))
+app.listen(process.env.PORT || port, () => console.log(`App running on http://localhost:${port}`))
